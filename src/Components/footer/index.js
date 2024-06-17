e8fb9db267408d65ff093386f3c6b559a9df70ee
Tushar Khandelwal <khandelwaltushar2002@gmail.com>
2024-06-17 23:05:01 +0530

    ui updated

--- src/Components/footer/index.js
+++ src/Components/footer/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-white py-6">
+    <footer className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-white py-6 mt-6">
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex flex-col justify-center md:justify-start">
-          <p className="mb-4">&copy; {new Date().getFullYear()} Hackon Team Dynamo from NIT Jalandhar. All rights reserved.</p>
+          <p className="mb-4">&copy; {new Date().getFullYear()} Hackon Team Dynamo (NIT Jalandhar)</p>
           <div className="space-x-4">
             <a href="/privacy" className="text-gray-400 hover:text-gray-200">Privacy Policy</a>
             <a href="/about" className="text-gray-400 hover:text-gray-200">Terms of Service</a>
