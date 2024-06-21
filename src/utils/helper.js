24a3932b5095c9e8ba911780aa71a4cc0aa6393c
Tushar Khandelwal <khandelwaltushar2002@gmail.com>
2024-06-21 20:42:01 +0530

    folder structure updated_2

--- src/utils/helper.js
+++
@@ -1,8 +0,0 @@
-export const categoryLabels = [ // Array of category labels for clarity
-    "General Inquiries",
-    "AWS Support",
-    "Online Retailer",
-    "Refund Requests",
-    "Seller Support",
-    "Prime Member Services",
-  ];
